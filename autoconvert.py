a3e906491406a76e981f8a0ff3654b7b09a6541c
Felix Prescott <57864637+downline@users.noreply.github.com>
2020-01-05 20:06:48 +0500

    Version 2.1
    Optimized and added sample rate change, since DYNAMO can't play audio with higher sr than 44100

--- autoconvert.py
+++ autoconvert.py
@@ -3,29 +3,23 @@ from math import ceil
 import glob
 
 songlist = [f for f in glob.glob("songs/*.mp3")]
-
 i = 0
 while i < len(songlist):
     try:
-        songfile = songlist[i]
-        songfileformatted = songfile.replace('\\', '/')
-        song = AudioSegment.from_file(songfileformatted)
-        songfaded = song.fade_in(7000).fade_out(7000)
-        
-        songfaded.duration_seconds == (len(song) / 1000.0) 
-        songduration = ceil(songfaded.duration_seconds)
-        print("Duration(SEC): ", songduration)
-        
-        songname0 = songfileformatted.replace("songs/", "")
-        songname1 = songname0.replace(".mp3", "")
-        songname2 = songname1.replace(".", "")
-        songname3 = songname2 + " _" + str(songduration) + ".ogg"
-        songpathname = "converted_songs/" + songname3
-        
-        print("Path and name: ", songpathname)
-        songfaded.export(songpathname, format="ogg")
-        print("Converted, doing the next one.")
-        i += 1
+        file = songlist[i].replace('\\', '/')
+        song = AudioSegment.from_file(file)
+        editedsong = song.fade_in(7000).fade_out(7000).set_frame_rate(44100)
+        editedsongduration = ceil((len(editedsong) / 1000.0))
+        oldfilename = file.replace("songs/", "").replace(".mp3", "").replace(".", "").replace(",", "")
+        filename = oldfilename + " _" + str(editedsongduration) + ".ogg"
+        filepath = "converted_songs/" + filename
+        print("Converting: ", filename)
+        editedsong.export(filepath, format="ogg")
+        i +=1
+        if i == len(songlist):
+             input("Success. Out of the songs. Press enter to exit.")
+        else:
+            print("Success. Next.")
     except Exception as e:
-        print("Oopsie, something bad happened!", e)
-
+        print("Wow, something broke: ", e)
+        input()
