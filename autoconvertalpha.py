c5ee1fbe4a868f8496e83a65367bfbe244e577a1
Felix Prescott <57864637+downline@users.noreply.github.com>
2020-02-23 23:29:14 +0500

    Little tiny change
    I thought of another little optimization when writing README.

--- autoconvertalpha.py
+++ autoconvertalpha.py
@@ -8,11 +8,9 @@ fileext = ".mp3"
 for i in glob.glob("songs/*" + fileext):
     try:
         song = AudioSegment.from_file(i).fade_in(7000).fade_out(7000).set_frame_rate(44100)
-        filename = i.replace("songs\\", "").replace(fileext, "").replace(".", "").replace(",", "").replace("_", "") + " _" + str(ceil((len(song) / 1000.0))).zfill(3)
-        filepath = "converted_songs/" + filename
-        print("Converting:", filename)
+        filepath = "converted_songs/" + i.replace("songs\\", "").replace(fileext, "").replace(".", "").replace(",", "").replace("_", "") + " _" + str(ceil((len(song) / 1000.0))).zfill(3)
         song.export(filepath + ".ogg", format="ogg")
+        print("Saved: " + filepath)
     except Exception as e:
         print("Wow, something broke: ", e)
         input()
-
